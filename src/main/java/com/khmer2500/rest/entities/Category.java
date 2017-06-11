b92b8b64a95f67d6619aea8fb26d141d19843939
Chivon <chhaichivon@2ntkh.com>
2017-06-11 08:35:22 +0700

    create category entity

--- src/main/java/com/khmer2500/rest/entities/Category.java
+++ src/main/java/com/khmer2500/rest/entities/Category.java
@@ -1,5 +1,7 @@
 package com.khmer2500.rest.entities;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +14,10 @@ import javax.persistence.Table;
 public class Category {
 
 	private Long id;
+	private String name;
+	private String description;
+	private Date date;
+	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,5 +25,38 @@ public class Category {
 	public Long getId() {
 		return id;
 	}
-	
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name ="tb_cat_name")
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "tb_cat_dis")
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Column(name ="tb_cat_date")
+	public Date getDate() {
+		return date;
+	}
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
