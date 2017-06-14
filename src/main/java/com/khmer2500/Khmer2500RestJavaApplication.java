a9a255b77e929e565d1213b1ca49b09f3d41115a
chhaichivon <chivonchhai@hotmail.com>
2017-06-15 06:35:47 +0700

    resolve error configuration

--- src/main/java/com/khmer2500/Khmer2500RestJavaApplication.java
+++ src/main/java/com/khmer2500/Khmer2500RestJavaApplication.java
@@ -1,8 +1,16 @@
 package com.khmer2500;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@EnableScheduling
 @SpringBootApplication
 public class Khmer2500RestJavaApplication {
 
